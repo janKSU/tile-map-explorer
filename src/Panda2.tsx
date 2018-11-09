f121ba0135837d62478fead66ba0fa7ab6a01d51
Jan Bednář <bednaj14@fel.cvut.cz>
2018-11-08 21:42:31 -0600

    Final files release

---
+++ src/Panda2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Panda2" tilewidth="32" tileheight="32" tilecount="40" columns="5">
+ <image source="Panda2.png" width="160" height="256"/>
+</tileset>
